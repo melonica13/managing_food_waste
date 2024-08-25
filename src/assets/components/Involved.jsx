785de77d0b3a0c0c09d52c2b7e39f27226526e90
melonica13 <159702529+melonica13@users.noreply.github.com>
2024-08-25 19:03:41 +0100

    changed manage into reduce

--- src/assets/components/Involved.jsx
+++ src/assets/components/Involved.jsx
@@ -9,7 +9,7 @@ const GetInvolvedSection = () => {
           Join our growing community of food waste warriors and make a difference! Here's how you can get involved:
         </p>
         <ul className="get-involved-list">
-          <li>Become a Foodify Partner: If you're a restaurant or food supplier, partner with us to manage food waste.</li>
+          <li>Become a Foodify Partner: If you're a restaurant or food supplier, partner with reduce food waste.</li>
           <li>Volunteer: Help us in our food rescue missions and community outreach programs.</li>
           <li>Spread the Word: Share our mission on social media and with your friends and family.</li>
           <li>Donate: Support our cause with a one-time or recurring donation.</li>
